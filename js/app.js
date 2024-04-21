2d592cb9b6963a8afbba336e61afb637103dd022
Jafr200 <julianandredflorezrios@gmail.com>
2024-04-21 13:53:34 -0500

    Agregamos el js con el codigo para ejecutar la calculadora

--- js/app.js
+++ js/app.js
@@ -1,16 +1,51 @@
-text = "Hola, mundo"
-
-if(text.length >0){
-    let textoNuevo = ""
-
-    for(let i = 0; i < text.length - 1; i++){
-        textoNuevo += text[i];
-    }
-    text = textoNuevo
-}
+let resultado = document.querySelector(".calculadora-salida");
+let botones = document.querySelectorAll(".btn");
+let ultimoApretado = null;
 
 
-array = [1,2,3,4,5]
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
+        // if(resultado.textContent.length === 14){
+        //     resultado.textContent = resultado.textContent.slice(0, -1)
+        //     return
+        // }
 
-let valorNuevos = array.slice(-1)
-console.log(valorNuevos)
+        if(boton.id === "igual" && resultado.textContent.length > 0){
+            resultado.textContent = eval(resultado.textContent)
+            return
+        }
+
+        if(boton.classList.contains("signo")){
+            if(resultado.textContent === "0"){
+                resultado.textContent = "0";
+                return
+            }
+            if(ultimoApretado == "btn signo"){
+                console.log(ultimoApretado + " " + boton.textContent)
+                resultado.textContent = resultado.textContent.slice(0, -1);
+                resultado.textContent += botonApretado
+                return
+            }
+        }
+        if(boton.id === "c"){
+            resultado.textContent = "0"
+            return
+       }
+       if(boton.id === "borrar"){
+            if(resultado.textContent.length === 1){
+                resultado.textContent = "0"
+            } else{
+                resultado.textContent = resultado.textContent.slice(0, -1)
+            }
+            return
+        }
+        if(resultado.textContent == "0"){
+            resultado.textContent = botonApretado;
+        }
+        else{
+            resultado.textContent += botonApretado;
+        }
+        ultimoApretado = boton.className;
+    })
+});
